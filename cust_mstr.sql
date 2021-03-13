e19e80da6f2247f3d0b19152dade467af59adcd9
mahin651 <53258123+mahin651@users.noreply.github.com>
2021-03-13 18:34:10 +0530

    Create cust_mstr.sql

---
+++ cust_mstr.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cust_mstr_1805969(
+  cust_no varchar2(30) primary key,
+  fname varchar2(30),
+  mname varchar2(30),
+  lname varchar2(30),
+  dob_inc timestamp,
+  occup varchar2(30),
+  photograph varchar2(30),
+  signature varchar2(30),
+  pancopy varchar2(30),
+  form60 varchar2(30)
+);
